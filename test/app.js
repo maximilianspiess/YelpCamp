e588784a46c22a6a09b0aee7ed123d9e99ee4fa6
maximilianspiess <m.spiess.henning@gmail.com>
2022-04-19 22:59:13 +0200

    425

--- test/app.js
+++ test/app.js
@@ -9,8 +9,21 @@ app.use((req, res, next) => {
     next();
 })
 
+app.use('/dogs', (req, res, next) => {
+    console.log("GOTTA LOVE'EM DAAAAWGS")
+    next();
+})
 
-app.get('/', (req, res) => {
+const verify_pass = (req, res, next) => {
+    const { password } = req.query;
+    if (password === 'veganyay') {
+        next();
+    }
+    res.send('Sorry, you need a password (bitch...)');
+}
+
+
+app.get('/dogs', (req, res) => {
     res.send("Ich wünsch dir gute Reise...");
 })
 
@@ -18,6 +31,14 @@ app.get('/reise', (req, res) => {
     res.send("...wohin es dich auch trägt...");
 })
 
+app.get('/secret', verify_pass, (req, res) => {
+    res.send("Sometimes I sit in the shower for 10 minutes because I like it");
+})
+
 app.listen(3000, () => {
     console.log("App running on localhost:3000");
+})
+
+app.use((req, res) => {
+    res.send('NOT FOUND!')
 })
