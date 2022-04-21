52c95d9b048ed99104cf0724331fad108ec17498
maximilianspiess <m.spiess.henning@gmail.com>
2022-04-21 23:21:35 +0200

    add boilerplate, image and description

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places, descriptors} = require('./helpers');
+const { places, descriptors } = require('./helpers');
 const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -12,15 +12,19 @@ db.once("open", () => {
 });
 
 
-const sample = array => array[Math.floor(Math.random()*array.length)]
+const sample = array => array[Math.floor(Math.random() * array.length)]
 
-const seedDB = async() => {
+const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++){
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor((Math.random() * 20) + 10)
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Monocle ipsum dolor sit amet marylebone soft power business class ryokan. Exquisite ryokan punctual bulletin impeccable Ginza. Sophisticated remarkable smart, lovely tote bag Sunspel Lufthansa bureaux joy elegant Melbourne extraordinary vibrant ANA. Winkreative delightful signature, Muji impeccable efficient Scandinavian Shinkansen destination. Soft power the highest quality extraordinary, elegant signature Baggu Lufthansa airport sleepy Shinkansen eclectic tote bag.',
+            price
         })
         await camp.save();
     }
