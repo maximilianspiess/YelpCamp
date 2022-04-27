af7180e1af7a9a34eb3098bbb99c3c972824627f
maximilianspiess <m.spiess.henning@gmail.com>
2022-04-27 21:38:05 +0200

    437

--- test/app.js
+++ test/app.js
@@ -19,24 +19,39 @@ const verify_pass = (req, res, next) => {
     if (password === 'veganyay') {
         next();
     }
-    res.send('Sorry, you need a password (bitch...)');
+    // res.send('Sorry, you need a password (bitch...)');
+    throw new Error("Password required")
 }
 
+app.get('/', (req, res) => {
+    res.send("This da homepage mdfk")
+})
 
 app.get('/dogs', (req, res) => {
-    res.send("Ich wünsch dir gute Reise...");
+    res.send("Woof woof");
 })
 
 app.get('/reise', (req, res) => {
-    res.send("...wohin es dich auch trägt...");
+    res.send("We're going on a trip");
 })
 
 app.get('/secret', verify_pass, (req, res) => {
     res.send("Sometimes I sit in the shower for 10 minutes because I like it");
 })
 
+app.get('/mistake', (req, res) => {
+    tofu.eat()
+})
+
 app.listen(3000, () => {
     console.log("App running on localhost:3000");
+})
+
+app.use((err, req, res, next) => {
+    console.log("YAAAAAAAAAAAAAAAAAAAAAAAAAY")
+    console.log("YAAAAAAAAAASHITAAAAAAAAAAAY")
+    console.log("YAAAAAAAAAAAAAAAAAAAAAAAAAY")
+    next(err);
 })
 
 app.use((req, res) => {
