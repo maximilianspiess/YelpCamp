a4963336e8ed4310c5ce27e35b6c149fdcf8da94
maximilianspiess <m.spiess.henning@gmail.com>
2022-07-06 22:01:05 +0200

    add form validation to make easier to spot mistakes

--- test/app.js
+++ test/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const AppError = require('./AppError');
 
 
 app.use(morgan('dev'));
@@ -20,40 +21,50 @@ const verify_pass = (req, res, next) => {
         next();
     }
     // res.send('Sorry, you need a password (bitch...)');
-    throw new Error("Password required")
+    
+    throw new AppError("Password required", 401)
 }
 
 app.get('/', (req, res) => {
-    res.send("This da homepage mdfk")
+    res.send("This is da homepage")
 })
 
 app.get('/dogs', (req, res) => {
-    res.send("Woof woof");
+    res.send("Woof woof")
 })
 
-app.get('/reise', (req, res) => {
-    res.send("We're going on a trip");
+app.get('/error', (req, res) => {
+    chicken.fly()
 })
 
 app.get('/secret', verify_pass, (req, res) => {
-    res.send("Sometimes I sit in the shower for 10 minutes because I like it");
+    res.send("Sometimes I sit in the shower for 10 minutes because I like it")
 })
 
 app.get('/mistake', (req, res) => {
     tofu.eat()
 })
 
-app.listen(3000, () => {
-    console.log("App running on localhost:3000");
+app.get('/admin', (req, res) => {
+    throw new AppError('You are not an admin', 403)
 })
 
+// app.use((err, req, res, next) => {
+//     console.log("YAAAAAAAAAAAAAAAAAAAAAAAAAY")
+//     console.log("YAAAAAAAAAASHITAAAAAAAAAAAY")
+//     console.log("YAAAAAAAAAAAAAAAAAAAAAAAAAY")
+//     next(err);
+// })
+
 app.use((err, req, res, next) => {
-    console.log("YAAAAAAAAAAAAAAAAAAAAAAAAAY")
-    console.log("YAAAAAAAAAASHITAAAAAAAAAAAY")
-    console.log("YAAAAAAAAAAAAAAAAAAAAAAAAAY")
-    next(err);
+    const { status = 500, message = 'Something went wrong' } = err;
+    res.status(status).send(message)
 })
 
 app.use((req, res) => {
-    res.send('NOT FOUND!')
+    res.status(404).send('NOT FOUND!')
+})
+
+app.listen(3000, () => {
+    console.log("App running on localhost:3000");
 })
